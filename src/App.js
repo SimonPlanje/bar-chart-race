a45c39b1a4c08f957666c1fa75ecd25685a870a1
Simon <simon.planje2000@gmail.com>
2021-02-03 22:41:37 +0100

    trying to steal lars zn barchartrace

--- src/App.js
+++ src/App.js
@@ -6,11 +6,21 @@ import fetchData from './helper/data'
 function App() {
 
   const [barRaceState, setbarRaceState] = useState(null)
+  const [eventState, setEventState] = useState(null)
 
   
   useEffect(() => {
-    fetchData(setbarRaceState)
+    fetchData(setbarRaceState, setEventState)
       }, [])
+
+      useEffect(() => {
+        localStorage.setItem('data', JSON.stringify(barRaceState))
+      }, [barRaceState])
+
+    useEffect(() => {
+      localStorage.setItem('eventData', JSON.stringify(eventState))
+    }, [eventState])
+  
 
 
   return (
--- src/helper/data.js
+++ src/helper/data.js
@@ -2,24 +2,37 @@ import * as d3 from "d3"
 
 
 
-const fetchData = (setbarRaceState) =>{
+const fetchData = (setbarRaceState, setEventState) =>{
 
     const daySpendDataURL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSpwttasVjXReLn69FtOXSrwWxYjKqVSKiRenwS9xU76b_-NX6-YRYfRQEwm0ipV3sLAiNPrCz3uO7D/pub?gid=161211961&single=true&output=csv"
+    const eventDashboardURL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vS0ispSJft5GVxwMtalNZLqlwrk6j9Ig2azCOlGp0IGfrDbKTFBinaOpgGou1Nyz-_w2-sIqii0_DwK/pub?gid=0&single=true&output=csv"
 
+    d3.csv(eventDashboardURL).then(data => {
+        console.log(data)
+        setEventState(data)
+    })
 
-async function fetch(url) {
-    const data = await d3.csv(url)
-    setbarRaceState(data)
-    console.log(data)
-}
+    d3.csv(daySpendDataURL).then(data => {
+        console.log(data)
+        setbarRaceState(data)
+    })
 
-async function dayData() {
-    const dayDashboard = await fetch(daySpendDataURL);
-    console.log(dayDashboard)
-    return dayDashboard;
-}
+// async function fetch(url) {
+//     const data = await d3.csv(url)
+//     console.log(data)
 
-dayData()
+//     return data
+// }
+
+// fetch(daySpendDataURL)
+
+// async function dayData() {
+//     const response = await fetch(daySpendDataURL);
+//     // console.log(response)
+//     // return response;
+// }
+
+// dayData()
 
 
 } 
