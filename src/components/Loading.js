d2da817aae6f189fb0e1417358dfd86462ada64c
Simon <simon.planje2000@gmail.com>
2021-02-11 12:12:46 +0100

    added loading screen barchart

---
+++ src/components/Loading.js
@@ -0,0 +1,20 @@
+import React, { useEffect, useState} from 'react'
+
+
+function Loading() {
+
+
+
+
+
+  return (
+    <div className="App">
+   <h1> LOADING BARCHART... </h1>
+    </div>
+  )
+  
+
+
+}
+
+export default Loading;
